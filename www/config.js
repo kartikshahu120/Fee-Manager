8c2ee58aadbfbef23d6537d52507a04f054c7d9b
Kartik <kartikshahu120@gmail.com>
2026-07-19 15:44:49 +0530

    exam portal is conneded

--- www/config.js
+++ www/config.js
@@ -1,4 +1,4 @@
 // config.js
 const SCRIPT_URL = "https://script.google.com/macros/s/AKfycbzsXgbHIS7cYllgA_U9idO6eQtGODS5GH_MQNEkrPWqJNJt3ftB8GwbRw66H8Ap947O6g/exec";
 
-const RESULT_SCRIPT_URL = "PASTE_YOUR_RESULTS_DEPLOY_LINK_HERE";
+const RESULT_SCRIPT_URL = "https://script.google.com/macros/s/AKfycbwk5sE-HhXP3DXbboxQHPYpU4i3fsLh614nilXvXJkiFSvpvUyioSzWsbNTLNjKtHvR/exec";
